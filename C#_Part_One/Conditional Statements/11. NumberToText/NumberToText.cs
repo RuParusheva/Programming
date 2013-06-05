c30877b3caa5c4632bad24f6b85bcfa8f4a6b80b
Ru Parusheva <ruparusheva@gmail.com>
2013-06-05 05:54:35 +0300

    post

--- C#_Part_One/Conditional Statements/11. NumberToText/NumberToText.cs
+++ C#_Part_One/Conditional Statements/11. NumberToText/NumberToText.cs
@@ -7,19 +7,70 @@ Examples:
 711 -> "Seven hundred and eleven"*/
 
 using System;
+using System.Collections;
 
 class NumberToText
 {
     static void Main()
     {
-        int[] set = new int[1000];
+        Console.Write("Enter integer number: ");
+        uint userInput;
+        bool isParsed = uint.TryParse(Console.ReadLine(), out userInput);
 
-        int counter = 0;
+        string[] exceptions = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
 
-        for (int i = 0; i < 1000; i++)
+        string[] tens = { "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+        string text = "";
+
+        if (isParsed)
         {
-            Console.Write("Enter integer number: ");
-            set[i] = Convert.ToInt32(Console.ReadLine());
+            if (userInput == 0)
+            {
+                Console.WriteLine("Zero");
+            }
+            else if (userInput > 999)
+            {
+                Console.WriteLine("You are trying to convert a value which exceeds 999. Please try a different number. ");
+            }
+            else
+            {
+                if (userInput > 99 && userInput < 1000)
+                {
+                    if (userInput % 100 == 0)
+                    {
+                        uint result = userInput / 100;
+                        text = text + exceptions[result - 1] + " hundred ";
+                    }
+                    else if (userInput % 100 == 0 && userInput % 10 == 0)
+                    {
+                        uint result = userInput / 100;
+                        uint resultOne = userInput / 10;
+                        text = text + exceptions[result - 1] + " hundred " + tens[resultOne - 1];
+                    }
+                    else
+                    {
+                        Console.WriteLine("blank");
+                    }
+                }
+
+                else if (userInput > 19 && userInput < 100)
+                {
+                    uint result = userInput / 10;
+                    text = text + tens[result - 1] + " ";
+                    userInput = userInput % 10;
+                }
+
+                if (userInput > 0 && userInput< 20)
+                {
+                    text = text + exceptions[userInput - 1];
+                }
+                Console.WriteLine(text);
+            }
+        }
+        else
+        {
+            Console.WriteLine("You have entered a value which is not in scope for this program. Try again! ");
         }
     }
 }
